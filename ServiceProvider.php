f0ec5fa5e0eb2ca21924629485711b74690f7856
宇井 陸登 <ui@appleple.com>
2023-04-10 16:41:09 +0900

    build: 開発環境を整理

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -1,38 +1,61 @@
 <?php
+
 namespace Acms\Plugins\GoogleSheets;
 
 use ACMS_App;
 use Acms\Services\Common\HookFactory;
 use Acms\Services\Common\InjectTemplate;
-use Acms\Services\Facades\Storage;
 
 class ServiceProvider extends ACMS_App
 {
-    public $version     = '1.0.0';
-    public $name        = 'Google Sheets';
-    public $author      = 'com.appleple';
-    public $module      = false;
-    public $menu        = 'google_sheets_index';
-    public $desc        = 'フォームの内容を Google スプレッドシートに流し込むためのアプリです。';
+    /**
+     * @var string
+     */
+    public $version = '1.0.0';
+
+    /**
+     * @var string
+     */
+    public $name = 'Google Sheets';
+
+    /**
+     * @var string
+     */
+    public $author = 'com.appleple';
+
+    /**
+     * @var bool
+     */
+    public $module = false;
+
+    /**
+     * @var string
+     */
+    public $menu = 'google_sheets_index';
+
+    /**
+     * @var string
+     */
+    public $desc = 'フォームの内容を Google スプレッドシートに流し込むためのアプリです。';
 
     /**
      * サービスの起動処理
      */
     public function init()
     {
-        require_once dirname(__FILE__).'/vendor/autoload.php';
+        require_once dirname(__FILE__) . '/vendor/autoload.php';
         $hook = HookFactory::singleton();
         $hook->attach('GoogleSheets', new Hook);
         $inject = InjectTemplate::singleton();
 
         if (ADMIN === 'app_google_sheets_index') {
-            $inject->add('admin-topicpath', PLUGIN_DIR . 'GoogleSheets/theme/topicpath.html');
-            $inject->add('admin-main', PLUGIN_DIR . 'GoogleSheets/theme/index.html');
-        } else if (ADMIN === 'app_google_sheets_callback') {
-            $inject->add('admin-topicpath', PLUGIN_DIR . 'GoogleSheets/theme/topicpath.html');
-            $inject->add('admin-main', PLUGIN_DIR . 'GoogleSheets/theme/callback.html');
+            $inject->add('admin-topicpath', PLUGIN_DIR . 'GoogleSheets/template/admin/topicpath.html');
+            $inject->add('admin-main', PLUGIN_DIR . 'GoogleSheets/template/admin/main.html');
+        } elseif (ADMIN === 'app_google_sheets_callback') {
+            $inject->add('admin-topicpath', PLUGIN_DIR . 'GoogleSheets/template/admin/topicpath.html');
+            $inject->add('admin-main', PLUGIN_DIR . 'GoogleSheets/template/admin/callback.html');
         }
-        $inject->add('admin-form', PLUGIN_DIR . 'GoogleSheets/theme/form.html');
+        $inject->add('admin-form', PLUGIN_DIR . 'GoogleSheets/template/admin/form.html');
     }
     /**
      * インストールする前の環境チェック処理
@@ -52,7 +75,6 @@ class ServiceProvider extends ACMS_App
      */
     public function install()
     {
-
     }
 
     /**
